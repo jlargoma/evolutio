1f2b4474d346b484841d77e19be9ec9b66867a26
PinGoDev <pingodevweb@gmail.com>
2020-03-24 18:23:33 -0300

    add gitignore

---
+++ resources/views/backend/contabilidad/index.blade.php
@@ -0,0 +1,17 @@
+@extends('backend.base')
+
+@section('content')
+
+<div class="container-fluid">
+  <div class="fade-in">
+    <div class="row">
+
+    </div>
+  </div>
+</div>
+
+@endsection
+
+@section('javascript')
+
+@endsection
